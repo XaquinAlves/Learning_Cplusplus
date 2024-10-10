6bbf582c12c20a96b10654f183606452479a4548
XaquinAlves <xaquinalves@gmaill.com>
2024-10-10 18:10:55 +0200

    First Commi

---
+++ C-C++/miPrimerPrograma/holamundo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+/*
+Programa creado por Xaquin Alves Gonzalez
+*/ 
+int main(){
+    cout << "Hola mundo!"; //Recordar saludar en varios idiomas
+
+    return 0;
+}
