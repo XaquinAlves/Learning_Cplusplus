6ec19f77c379b75c6c99ea5564d8267570350855
XaquinAlves <xaquinalves@gmaill.com>
2024-10-10 18:18:59 +0200

    Second Comit

---
+++ C-C++/miPrimerPrograma/holamundo2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hola mundo!" << endl;
+    cout << "Hello world" << endl;
+    cout << "Hallo welt" << endl;
+    cout << "Ciao mondo" << endl;
+    cout << "Ola mundo" << endl;
+    cout << "Salut Monde" << endl;
+
+    return 0;
+}
